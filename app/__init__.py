39c63f6cbaed47c5fdc5e59da1d9a9fbcc72908d
edil3ra <edil3ra@gmail.com>
2017-08-10 14:39:35 +0200

    install and configure debug toolbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from config import config
 
+from flask_debugtoolbar import DebugToolbarExtension
 
+toolbar = DebugToolbarExtension()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
+    
+    toolbar.init_app(app)
+    
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
--- config.py
+++ config.py
@@ -9,12 +9,16 @@ class Config:
     
     @staticmethod
     def init_app(app):
-        '''for post init after the extension are loaded'''
+        '''after the basic setting you can access the the app instancec'''
         pass
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    DEBUG_TB_ENABLED = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    DEBUG_TB_PROFILER_ENABLED = True
+    DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
