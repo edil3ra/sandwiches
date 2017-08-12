07deb47f392b16dee00634fbb602bf3cc0278509
vince <edil3ra@gmail.com>
2017-08-12 17:51:08 +0200

    Make the form display the errors

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,8 +6,8 @@ from wtforms import ValidationError
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField('Type your Email', validators=[Required(), Length(1, 64),
                                              Email()])
-    password = PasswordField('Password', validators=[Required()])
+    password = PasswordField('Type your Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
